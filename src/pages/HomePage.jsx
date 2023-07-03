cf3c462a3df3315d40222e8fb1ccb9f948bef301
adelintang <adelintang157@gmail.com>
2023-07-03 21:13:05 +0700

    ui homepage logic

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import { useState } from 'react'
+import Register from '../components/Register';
+import Login from '../components/Login';
 
 const HomePage = () => {
 
-  const [route, setRoute] = useState("")
+  const [route, setRoute] = useState(true);
+
+  const setLogin = () => {
+    setRoute(true);
+  }
+  
+  const setRegister = () => {
+    setRoute(false);
+  }
+  
 
   return (
-    <div>HomePage</div>
+    <div className='w-[80%] mx-auto bg-[#333] text-white p-4'>
+      <div className='w-[50%] mx-auto flex justify-around p-3'>
+        <button onClick={setLogin} className='bg-blue-300 p-4'>Login</button> 
+        <button onClick={setRegister} className='bg-blue-300 p-4'>Register</button>
+      </div>
+      { route ? (<Login />) : (<Register />)}
+    </div>
   )
 }
 
